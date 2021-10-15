59a2d82ed90ee48f12da3f59c6c0f09a86300217
Younsoo Tom Lim <tomandlim@gmail.com>
2021-10-15 23:21:03 +0900

    Add: westagram

---
+++ assets/js/login.js
@@ -0,0 +1,38 @@
+/*Login Page*/
+const loginBttn = document.getElementById('submit');
+const thisIsUser = document.getElementById('username');
+const thisIsPassword = document.getElementById('password');
+
+let inputUserID;
+let inputPasswords;
+
+
+const userIDChecker = e => {
+    inputUserID = e.target.value;
+    activateSubmit();
+}
+
+const passwordChecker = e => {
+    inputPasswords = e.target.value;
+    activateSubmit();
+}
+
+const activateSubmit = () => {
+    inputUserID.includes('@')&&inputPasswords.length > 5 ? loginBttn.classList.add("login-activate") : loginBttn.classList.remove("login-activate");
+}
+
+thisIsUser.oninput = userIDChecker;
+thisIsPassword.oninput = passwordChecker;
+
+// const activateBttn = () => {
+//     if(thisIsPassword.value && thisIsUser.value){
+//         loginBttn.classList.add("login-activate");
+//     }else{
+//         loginBttn.classList.remove("login-activate");
+//     }
+// }
+
+// thisIsUser.onkeyup = activateBttn;
+// thisIsPassword.onkeyup = activateBttn;
+
+// console.log(thisIsPassword)
---
+++ assets/js/main.js
@@ -0,0 +1,62 @@
+"use strict";
+// comments
+const commentTextID = document.getElementById("write-comment-text");
+const commentSubmit = document.getElementById("write-comment-submit");
+const commentBox = document.getElementById("post-comments-box#1");
+
+let inputComment;
+
+const commentAddHTML = () => {
+    const newDiv = document.createElement("div");
+    const newUsernameSpan = document.createElement("span");
+    const newCommentSpan = document.createElement("span");
+    const newHeartIcon = document.createElement("i");
+    
+    newDiv.className = "post-comments";
+    newUsernameSpan.className = "username";
+    newUsernameSpan.innerHTML = "mincraft_bangbang&nbsp;";
+    newDiv.appendChild(newUsernameSpan);
+
+    newCommentSpan.className = "comment-text";
+    newCommentSpan.innerHTML = inputComment;
+
+    newDiv.appendChild(newCommentSpan);
+
+    newHeartIcon.className = "heart-icon";
+    newHeartIcon.innerHTML = `<i class="far fa-heart"></i>`
+    newDiv.appendChild(newHeartIcon);
+
+    commentBox.appendChild(newDiv);
+}
+
+const commentAddTheWritesAndReset = e => {
+    commentAddHTML();
+    //!!
+    commentResetInputVl()
+}
+
+const commentResetInputVl = e => {
+    commentTextID.value = "";
+    inputComment = commentTextID.value;
+}
+
+const commentPressEnter = e => {
+    if (e.key === 13 || e.which === 13){
+        e.preventDefault();
+        commentAddTheWritesAndReset();
+        commentSubmit.classList.remove("activate-comment-submit")
+        return false;
+    }
+}
+
+const commentChecker = e => {
+    inputComment = e.target.value;
+    // console.log(inputComment);
+    inputComment ? commentSubmit.classList.add("activate-comment-submit") : commentSubmit.classList.remove("activate-comment-submit");
+}
+
+commentTextID.oninput = commentChecker;
+commentSubmit.onmouseup = commentAddTheWritesAndReset;
+commentTextID.addEventListener("keypress", commentPressEnter);
+
+//likebutton
